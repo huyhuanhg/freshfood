b4708fe670e3c9308c6af03a9c9468847927ddb1
huyhuanhg <huyhuanhg@gmail.com>
2021-09-24 21:39:26 +0700

    update url api

--- src/contants/backend.contant.js
+++ src/contants/backend.contant.js
@@ -1,3 +1,3 @@
-export const ROOT_PATH = 'http://127.0.0.1:8000';
-export const SERVER_ADMIN_API_URL = 'http://127.0.0.1:8000/api/v1/manager';
-export const SERVER_CLIENT_API_URL = 'http://127.0.0.1:8000/api/v1';
+export const ROOT_PATH = 'http://128.199.126.14';
+export const SERVER_ADMIN_API_URL = '128.199.126.14/api/v1/manager';
+export const SERVER_CLIENT_API_URL = 'http://128.199.126.14/api/v1';
